61f8237126e797587e770622166a5fa82821f4ab
Wonboyn <>
2021-02-05 17:06:03 +1100

    More terraform tweaks

---
+++ terraform/api_gateway.tf
@@ -0,0 +1,42 @@
+# ----------------------
+#
+# Setup API Gateway
+#
+# ----------------------
+
+##
+# Swagger definition for API Gateway
+###
+data "template_file" api_swagger {
+  template = file("./self-service-swagger.yaml")
+  vars     = {
+    ProcessRootBotMessage = aws_lambda_function.root_bot_lambda_function.invoke_arn
+  }
+}
+
+
+##
+# API Gateway Definition
+###
+resource "aws_api_gateway_rest_api" "api_gateway" {
+  name        = "SelfService"
+  description = "Slack Self Service API"
+  body        = data.template_file.api_swagger.rendered
+}
+
+
+##
+# API Gateway CloudWatch ARN
+###
+resource "aws_api_gateway_account" "api_gw_account" {
+  cloudwatch_role_arn = aws_iam_role.cloudwatch.arn
+}
+
+
+##
+# API Gateway CloudWatch ARN
+###
+resource "aws_api_gateway_deployment" "api_gateway_deployment" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  stage_name  = var.environment
+}
