8c205d1b76bd5f6d4dd98f86a872d0c4259404f5
Wonboyn <>
2021-02-19 16:07:54 +1100

    More terraform tweaks

---
+++ terraform/azure/botregistration.tf
@@ -0,0 +1,23 @@
+# ----------------------
+#
+# Setup Bot Registration
+#
+# ----------------------
+
+###
+# Root Bot
+###
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "root_bot_resource_group" {
+  name     = var.bot_registration_resource_group_name
+  location = var.azure_location
+}
+
+resource "azurerm_bot_channels_registration" "root_bot_registration" {
+  name                = var.bot_registration_name
+  location            = "global"
+  resource_group_name = azurerm_resource_group.root_bot_resource_group.name
+  sku                 = var.bot_registration_sku
+  microsoft_app_id    = "${data.azurerm_client_config.current.client_id}"
+}
